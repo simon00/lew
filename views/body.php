597c22684ba436f0dfd74a96bf9b06cfdcb24d99
Baoxiong <g.pauthion@gmail.com>
2014-07-31 02:38:41 +0800

    some text in front page

--- views/body.php
+++ views/body.php
@@ -1,6 +1,6 @@
 <div id="content-main" class="container">
 
-Something 1
+Languages is one of the most important thing in today's world. But it should be fun. Don't you think ? This is what learning should be about: fun! And what's more fun than meeting new friends from all over the world near your doorstep?
 
 </div>
 
@@ -8,6 +8,6 @@ Something 1
 
 <div id="content-secondary" class="container">
 
-Something 2
+With LEW you can not only find one on one partners to practice your languages, but you can also participate to many events. Check out our latest events!
 
 </div>
