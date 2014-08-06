5f5fbc9c2c789f20dd63482e02de46c95ecb06b4
simon00 <simongit00@gmail.com>
2014-08-06 15:59:43 +0200

    validator class

--- controlers/signin.ctrl.php
+++ controlers/signin.ctrl.php
@@ -4,9 +4,87 @@
 *   Sign-in Controler
 */
 
+class Validator {
+    
+    private $required = array();
+    private $optional = array();
+    
+    private $data;
+    
+    public $errors = array();
+    
+    public function __construct($data) {
+        
+        $this->data = $data; 
+    }
+    
+    public function required($key, $type, $length = 0) {
+    
+        $this->required[$key] = array('type' => $type, 'length' => $length);
+        
+    }
+    
+    public function optional($key, $type, $length = 0) {
+    
+        $this->optional[$key] = array('type' => $type, 'length' => $length);
+    
+    }
+    
+    public function validate() {
+        
+        foreach($this->required as $key => $value) {
+            
+            if ( isset($this->data[$key]) ) {
+            
+                if ( !self::evaluate($this->data[$key], $value['type'], $value['length']) ) {
+                
+                    array_push($this->errors, $key);
+                
+                }
+            
+            } else {
+            
+                array_push($this->errors, $key . ' 2 ');
+            
+            }
+            
+        }
+        
+        foreach($this->optional as $key => $value) {
+            
+            if ( isset($this->data[$key]) ) {
+            
+                if ( !self::evaluate($this->data[$key], $value['type'], $value['length']) ) {
+                
+                    array_push($this->errors, $key);
+                
+                }
+            
+            }
+            
+        }
+        
+    }
+    
+    public static function evaluate($val, $type, $length) {
+        
+        switch ($type) {
+            case 'text':
+                return true;
+                break;
+            case 'email':
+                return true;
+                break;
+            default:
+                return false;
+        }
+        
+    }
+    
+}
+
 require_once(MODELSPATH . 'users.php');
 require_once('db/connect.php');
-
 
 // if user is already logged in, redirect to main page
 if ( isset($_SESSION['user_id']) ) {
@@ -18,6 +96,7 @@ $m_user = new M_Users($db);
 $request = Flight::request();
 
 $values = array('username', 'password');
+$optional = array();
 
 $signin_data = array();
 $error = false;
@@ -30,6 +109,16 @@ foreach ( $values as $value ) {
 
 // Sign-up
 if ( $request->method === 'POST' ) {
+
+    $v = new Validator($request->data);
+    $v->required('username', 'text', 25);
+    $v->required('password', 'zd', 255);
+    
+    $v->validate();
+    
+    foreach ( $v->errors as $e ) {
+        Messages::add_error($e . ' invalide.');
+    }
     
     // Username
     if ( isset($request->data['username']) ) {
